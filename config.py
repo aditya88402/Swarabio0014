011e8f2013893bb7e2aac3855ae2fbba327650d8
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2025-07-21 23:31:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", 12345))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID", 123456789))
+MONGO_URI = os.getenv("MONGO_URI")
+MUTE_DURATION_HOURS = int(os.getenv("MUTE_DURATION_HOURS", 4))
