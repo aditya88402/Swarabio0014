9e581335b8ff15d824cd4c1ab2c7c036250e05ba
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2025-07-21 23:29:46 +0530

    Create start.py

---
+++ Biolink/plugins/start.py
@@ -0,0 +1,16 @@
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+def register(app):
+    @app.on_message(filters.command("start") & filters.private)
+    async def start(_, message: Message):
+        await message.reply_text(
+            f"👋 Hello {message.from_user.first_name}, welcome to Bio Mute Bot!",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton("➕ Add me to your group", url="https://t.me/YourBotUsername?startgroup=true")],
+                    [InlineKeyboardButton("📢 Update Channel", url="https://t.me/YourChannel")],
+                    [InlineKeyboardButton("❓ Help", callback_data="help")]
+                ]
+            )
+        )
